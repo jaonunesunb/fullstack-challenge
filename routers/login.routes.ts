76d7110cf1b9313b91881496a7da4887a2fa8469
jaonunesunb <190109831@aluno.unb.br>
2023-03-26 13:16:24 -0300

    fix:directory error

--- routers/login.routes.ts
+++
@@ -1,9 +0,0 @@
-import { createSessionController } from "../controllers/login/login.controller";
-
-const { Router } = require("express");
-
-const loginRoutes = Router();
-
-loginRoutes.post("", createSessionController);
-
-export default loginRoutes;
--- routers/session.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createSessionController } from "../controllers/login/login.controller";
-
-const sessionRoutes = Router();
-
-sessionRoutes.post("/", createSessionController);
-
-export default sessionRoutes;
